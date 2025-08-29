d6f64f993e7d4becf493bb490f5d9ed6f3ba30ea
Vaishnavi Dayanand <116504504+VaishnaviDayanand@users.noreply.github.com>
2025-08-29 22:36:21 +0530

    Update summarizer.py

--- backend/summarizer.py
+++ backend/summarizer.py
@@ -194,20 +194,3 @@ if __name__ == "__main__":
     )
 
     print("\n🔹 Summary:\n", summary)
-[5/15, 8:56 PM] Vaishnavi Dayanand: import pdfplumber
-from docx import Document
-
-def read_pdf(file):
-    text = ""
-    with pdfplumber.open(file) as pdf:
-        for page in pdf.pages:
-            if page.extract_text():
-                text += page.extract_text() + "\n"
-    return text
-
-def read_docx(file):
-    doc = Document(file)
-    return "\n".join([para.text for para in doc.paragraphs])
-
-def read_txt(file):
-    return file.read().decode('utf-8')
